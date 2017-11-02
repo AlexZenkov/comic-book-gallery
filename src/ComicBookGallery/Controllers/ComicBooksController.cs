a893ba026a77f465e05c11b8a46ace028fb961eb
Alex <alex.zenkov@sonartechnologies.com>
2017-11-02 18:43:52 +1100

    Repository added

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,27 +1,18 @@
 ﻿using System.Web.Mvc;
-using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] {
-                    new Artist {Role = "Script", Name = "Dan Slott"},
-                    new Artist {Role = "Pencils", Name = "Humberto Ramos"},
-                    new Artist {Role = "Inks", Name = "Victor Olazaba"},
-                    new Artist {Role = "Colors", Name = "Edgar Delgado"},
-                    new Artist {Role = "Letters", Name = "Chris Eliopoulos"}
-                }
-            };
+        //public ActionResult Detail()
+        //{
+        //    return View(new ComicBookRepository().GetComicBook(0));
+        //}
 
-            return View(comicBook);
+        public ActionResult Detail(int? id)
+        {
+            return View(new ComicBookRepository().GetComicBook(id ?? 0));
 
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
             //    return Redirect("/");
