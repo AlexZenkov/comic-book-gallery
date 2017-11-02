dd1640a22c52241b1deea367425dc4ec9622b037
Alex <alex.zenkov@sonartechnologies.com>
2017-11-02 11:51:25 +1100

    Added Detail view

--- src/ComicBookGallery/App_Start/RouteConfig.cs
+++ src/ComicBookGallery/App_Start/RouteConfig.cs
@@ -18,11 +18,6 @@ namespace ComicBookGallery
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            //routes.MapRoute(
-            //    name: "Details",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "ComicBook", action = "Details", id = UrlParameter.Optional }
-            //);
         }
     }
 }
